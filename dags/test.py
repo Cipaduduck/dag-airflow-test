978db73a1d5863fbbce7d59ec8631a7cf3eb2986
Muhammad Arif Fahrudin <71691156+Cipaduduck@users.noreply.github.com>
2024-10-24 10:18:31 +0700

    Update test.py

--- dags/test.py
+++ dags/test.py
@@ -1,1 +1,32 @@
-print("hallo")
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def print_message():
+    print("This is a new DAG")
+
+default_args = {
+    'owner': 'sigma',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'Create_new_dag',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+)
+
+t1 = PythonOperator(
+    task_id='print_message',
+    python_callable=print_message,
+    dag=dag,
+)
+
+t1
